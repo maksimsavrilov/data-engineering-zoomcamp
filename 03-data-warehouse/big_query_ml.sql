9e3d0635624a5e21ebc1b9fde61101e763eb27a4
Maksim Savrilov <maxim.savrilov@gmail.com>
2026-07-31 13:25:58 +0300

    name replacement for local changes

--- 03-data-warehouse/big_query_ml.sql
+++ 03-data-warehouse/big_query_ml.sql
@@ -1,25 +1,25 @@
 -- SELECT THE COLUMNS INTERESTED FOR YOU
-SELECT passenger_count, trip_distance, PULocationID, DOLocationID, payment_type, fare_amount, tolls_amount, tip_amount
-FROM `taxi-rides-ny.nytaxi.yellow_tripdata_partitioned` WHERE fare_amount != 0;
+SELECT passenger_count, trip_distance, pickup_location_id, dropoff_location_id, payment_type, fare_amount, tolls_amount, tip_amount
+FROM `project-f2341c77-fecc-4b52-a12.zoomcamp_us.yellow_trips_partitioned` WHERE fare_amount != 0;
 
 -- CREATE A ML TABLE WITH APPROPRIATE TYPE
-CREATE OR REPLACE TABLE `taxi-rides-ny.nytaxi.yellow_tripdata_ml` (
+CREATE OR REPLACE TABLE `project-f2341c77-fecc-4b52-a12.zoomcamp_us.yellow_tripdata_ml` (
 `passenger_count` INTEGER,
 `trip_distance` FLOAT64,
-`PULocationID` STRING,
-`DOLocationID` STRING,
+`pickup_location_id` STRING,
+`dropoff_location_id` STRING,
 `payment_type` STRING,
 `fare_amount` FLOAT64,
 `tolls_amount` FLOAT64,
 `tip_amount` FLOAT64
 ) AS (
-SELECT passenger_count, trip_distance, cast(PULocationID AS STRING), CAST(DOLocationID AS STRING),
+SELECT passenger_count, trip_distance, cast(pickup_location_id AS STRING), CAST(dropoff_location_id AS STRING),
 CAST(payment_type AS STRING), fare_amount, tolls_amount, tip_amount
-FROM `taxi-rides-ny.nytaxi.yellow_tripdata_partitioned` WHERE fare_amount != 0
+FROM `project-f2341c77-fecc-4b52-a12.zoomcamp_us.yellow_trips_partitioned` WHERE fare_amount != 0
 );
 
 -- CREATE MODEL WITH DEFAULT SETTING
-CREATE OR REPLACE MODEL `taxi-rides-ny.nytaxi.tip_model`
+CREATE OR REPLACE MODEL `project-f2341c77-fecc-4b52-a12.zoomcamp_us.tip_model`
 OPTIONS
 (model_type='linear_reg',
 input_label_cols=['tip_amount'],
@@ -27,23 +27,23 @@ DATA_SPLIT_METHOD='AUTO_SPLIT') AS
 SELECT
 *
 FROM
-`taxi-rides-ny.nytaxi.yellow_tripdata_ml`
+`project-f2341c77-fecc-4b52-a12.zoomcamp_us.yellow_tripdata_ml`
 WHERE
 tip_amount IS NOT NULL;
 
 -- CHECK FEATURES
-SELECT * FROM ML.FEATURE_INFO(MODEL `taxi-rides-ny.nytaxi.tip_model`);
+SELECT * FROM ML.FEATURE_INFO(MODEL `project-f2341c77-fecc-4b52-a12.zoomcamp_us.tip_model`);
 
 -- EVALUATE THE MODEL
 SELECT
 *
 FROM
-ML.EVALUATE(MODEL `taxi-rides-ny.nytaxi.tip_model`,
+ML.EVALUATE(MODEL `project-f2341c77-fecc-4b52-a12.zoomcamp_us.tip_model`,
 (
 SELECT
 *
 FROM
-`taxi-rides-ny.nytaxi.yellow_tripdata_ml`
+`project-f2341c77-fecc-4b52-a12.zoomcamp_us.yellow_tripdata_ml`
 WHERE
 tip_amount IS NOT NULL
 ));
@@ -52,12 +52,12 @@ tip_amount IS NOT NULL
 SELECT
 *
 FROM
-ML.PREDICT(MODEL `taxi-rides-ny.nytaxi.tip_model`,
+ML.PREDICT(MODEL `project-f2341c77-fecc-4b52-a12.zoomcamp_us.tip_model`,
 (
 SELECT
 *
 FROM
-`taxi-rides-ny.nytaxi.yellow_tripdata_ml`
+`project-f2341c77-fecc-4b52-a12.zoomcamp_us.yellow_tripdata_ml`
 WHERE
 tip_amount IS NOT NULL
 ));
@@ -66,18 +66,18 @@ tip_amount IS NOT NULL
 SELECT
 *
 FROM
-ML.EXPLAIN_PREDICT(MODEL `taxi-rides-ny.nytaxi.tip_model`,
+ML.EXPLAIN_PREDICT(MODEL `project-f2341c77-fecc-4b52-a12.zoomcamp_us.tip_model`,
 (
 SELECT
 *
 FROM
-`taxi-rides-ny.nytaxi.yellow_tripdata_ml`
+`project-f2341c77-fecc-4b52-a12.zoomcamp_us.yellow_tripdata_ml`
 WHERE
 tip_amount IS NOT NULL
 ), STRUCT(3 as top_k_features));
 
 -- HYPER PARAM TUNNING
-CREATE OR REPLACE MODEL `taxi-rides-ny.nytaxi.tip_hyperparam_model`
+CREATE OR REPLACE MODEL `project-f2341c77-fecc-4b52-a12.zoomcamp_us.tip_hyperparam_model`
 OPTIONS
 (model_type='linear_reg',
 input_label_cols=['tip_amount'],
@@ -89,7 +89,7 @@ l2_reg=hparam_candidates([0, 0.1, 1, 10])) AS
 SELECT
 *
 FROM
-`taxi-rides-ny.nytaxi.yellow_tripdata_ml`
+`project-f2341c77-fecc-4b52-a12.zoomcamp_us.yellow_tripdata_ml`
 WHERE
 tip_amount IS NOT NULL;
 
--- 03-data-warehouse/extract_model.md
+++ 03-data-warehouse/extract_model.md
@@ -2,13 +2,12 @@
 [Tutorial](https://cloud.google.com/bigquery-ml/docs/export-model-tutorial)
 ### Steps
 - gcloud auth login
-- bq --project_id taxi-rides-ny extract -m nytaxi.tip_model gs://taxi_ml_model/tip_model
-- mkdir /tmp/model
-- gsutil cp -r gs://taxi_ml_model/tip_model /tmp/model
+- bq --project_id project-f2341c77-fecc-4b52-a12 extract -m zoomcamp_us.tip_model gs://tip_model/tip_model
+- mkdir %TMP%/model
+- gsutil cp -r gs://tip_model/tip_model %TMP%/model
 - mkdir -p serving_dir/tip_model/1
-- cp -r /tmp/model/tip_model/* serving_dir/tip_model/1
+- cp -r %TMP%/model/tip_model/* serving_dir/tip_model/1
 - docker pull tensorflow/serving
-- docker run -p 8501:8501 --mount type=bind,source=`pwd`/serving_dir/tip_model,target=
-  /models/tip_model -e MODEL_NAME=tip_model -t tensorflow/serving &
-- curl -d '{"instances": [{"passenger_count":1, "trip_distance":12.2, "PULocationID":"193", "DOLocationID":"264", "payment_type":"2","fare_amount":20.4,"tolls_amount":0.0}]}' -X POST http://localhost:8501/v1/models/tip_model:predict
+- docker run -p 8501:8501 --mount type=bind,source=`pwd`/serving_dir/tip_model,target=/models/tip_model -e MODEL_NAME=tip_model -t tensorflow/serving &
+- curl -d '{"instances": [{"passenger_count":1, "trip_distance":22.2, "pickup_location_id":"193", "dropoff_location_id":"264", "payment_type":"2","fare_amount":20.4,"tolls_amount":0.0}]}' -X POST http://localhost:8501/v1/models/tip_model:predict
 - http://localhost:8501/v1/models/tip_model
